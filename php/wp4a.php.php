d7aa4efa86b5a31835f3d51a1be202364133871f
RoshmiRoy <74670223+RoshmiRoy@users.noreply.github.com>
2021-03-06 15:22:50 +0530

    Add files via upload

---
+++ php/wp4a.php.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>get data </title>
+
+<body>
+
+<?php
+echo "Form no.:".$_POST["txtr_no"]."</br>";
+echo "Name:" .$_POST["txtname"]."</br>";
+
+echo "Address:".$_POST["add"]."</br>";
+echo "Contact No.:".$_POST["txtc_no"]."</br>";
+echo "Email ID:".$_POST["txteid"]."</br>";
+
+
+
+ ?>
+
+</body>
+</html>
